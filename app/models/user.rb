133e83499b4a09a4a34b6290ff76987fc96c174d
Eduard <ed.munteanu99@gmail.com>
2022-04-23 17:49:24 +0200

    Add user model

---
+++ app/models/user.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+class User < ApplicationRecord
+  devise :database_authenticatable, :recoverable, :registerable,
+         :rememberable, :trackable, :validatable
+end
